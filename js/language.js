1e4907d616cb69e5afafee30eaca983c847e87b2
TheAyos <ayosiiiofficiel@gmail.com>
2024-05-11 19:01:29 +0200

    [FIX] remove unnecessary code

--- js/language.js
+++ js/language.js
@@ -13,20 +13,10 @@ async function updateContentTo(lang) {
     document.querySelectorAll("[data-fr]").forEach((element) => {
         const frString = element.getAttribute("data-fr");
         const enString = element.getAttribute("data-en");
-        const frHtmlString = element.getAttribute("data-fr-html");
-        const enHtmlString = element.getAttribute("data-en-html");
-        if (frHtmlString && enHtmlString) {
-            if (lang === "fr") {
-                element.innerHTML = frHtmlString;
-            } else {
-                element.innerHTML = enHtmlString;
-            }
+        if (lang === "fr") {
+            element.textContent = frString;
         } else {
-            if (lang === "fr") {
-                element.textContent = frString;
-            } else {
-                element.textContent = enString;
-            }
+            element.textContent = enString;
         }
     });
 }
